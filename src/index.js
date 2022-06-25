87eab9323567918005dda876bc63720d840eb86f
Ajdin Imsirovic <ajdika@live.com>
2022-06-25 09:51:20 +0200

    Here's the problem with functional components at this point in time

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// JS file loaded by browser
+// App component gets created
+// We call geolocation service
+// App returns JSX, gets rendered to page as HTML
+// ...
+// We get result of geolocation!
+// TELL THE COMPONENT TO RERENDER ITSELF WITH THIS NEW INFORMATION
+
 const App = () => {
     window.navigator.geolocation.getCurrentPosition(
         (position) => console.log(position), // success callback
