2c2678625d84c030f860fe568ff72d087fdb528c
Ajdin Imsirovic <ajdika@live.com>
2022-06-25 09:32:27 +0200

    Add geolocation success and failure callbacks

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position), // success callback
+        (err) => console.log(err) // failure callback
+    )
+
     return <h1>Hi there!</h1>
 }
 
